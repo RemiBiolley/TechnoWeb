e0e4b9304d0b5ef01b9870613e3b1dc809b7f8e3
RemiBiolley <33449916+RemiBiolley@users.noreply.github.com>
2018-10-11 00:16:46 +0200

    Create ReadCookie.java

---
+++ Lab1/HelloWorldServlet/ReadCookie.java
@@ -0,0 +1,64 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ReadCookie
+ */
+@WebServlet("/ReadCookie")
+public class ReadCookie extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReadCookie() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		Cookie[] cookies = request.getCookies();
+
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>Lab1</TITLE></HEAD>");
+		out.println("<BODY>");
+		
+		out.println("<table>");
+		out.println("<tr>");
+		for (int i = 0; i < cookies.length; i++) {
+			out.println("<th>" + cookies[i].getName() + "</th>");
+		}
+		out.println("</tr>");
+		out.println("<tr>");
+		for (int i = 0; i < cookies.length; i++) {
+			out.println("<td>" + cookies[i].getValue() + "</td>");
+		}
+		out.println("</tr>");
+		out.println("</table>");
+		out.println("</BODY>");
+		out.println("</HTML>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
